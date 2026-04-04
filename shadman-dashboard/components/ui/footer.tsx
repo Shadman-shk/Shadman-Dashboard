a9f296f2bd04235a9b714afe354b5d72bac83220
Shadman <hossainshadman005@gmail.com>
2026-04-04 16:39:19 +1100

    Set up the Clerk Authentication and Completed Footer
    -Setup the sign in
    -Created the sign in page
    -Created the footer
    
    Attempted: Custom sign-in instead of model
    Issue: Signed In is not getting imported for being in different package
    Solution: Skipped it temporarily

--- shadman-dashboard/components/ui/footer.tsx
+++ shadman-dashboard/components/ui/footer.tsx
@@ -1,3 +1,21 @@
-export default function Footer(){
-    return <>Footer</>;
+export default function Footer() {
+    const currentYear = new Date().getFullYear();
+    return (
+        <footer className="border-t bg-background mt-auto">
+            <div className="container mx-auto px-4 py-4">
+                <div className="flex flex-col md:flex-row items-center justify-between gap-4">
+                    <div className="flex items-center gap-2 text-sm text-muted-forground">
+                        <span>Made with</span>
+                        <span>Shadman Hossain</span>
+                    </div>
+                    <div>
+                         <div className="flex items-center gap-4 text-sm text-muted-foreground">
+                            <span>{currentYear} Feedback Fusion. All rights reserved.</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+
 }
