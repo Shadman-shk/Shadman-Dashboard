8a14eda733583a66e61522ea903d9d41b713a82d
Shadman <hossainshadman005@gmail.com>
2026-04-23 00:10:16 +1000

    Add vote-based ordering to posts query
    -Modified Prisma findMany query to include author and votes.
    -Implemented sorting using votes _count in descending order.

--- shadman-dashboard/app/roadmap/page.tsx
+++ shadman-dashboard/app/roadmap/page.tsx
@@ -1,3 +1,18 @@
+import prisma from "@/lib/prisma";
 export default async function RoadmapPage() {
+
+    const posts = await prisma.post.findMany({
+            include: {
+                author: true,
+                votes: true,
+            },
+            orderBy: {
+                votes:{
+                    _count: "desc"
+                },
+            },
+        });
+
+        
     return <>Roadmap page</>;
 }
